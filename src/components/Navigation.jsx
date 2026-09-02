bb2c18dc8f2d04697903ee9cd6afdfcc3cbc9f27
itsprinsprays <prncbntz@gmail.com>
2026-09-02 20:19:56 +0800

    Icon Added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,4 @@
+import { FaShoppingCart, FaBox, FaUser } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
 export default function Navigation() {
@@ -7,8 +8,9 @@ export default function Navigation() {
         <>
         <div className="bg-[green] w-[200px] h-screen flex flex-col py-10 ">
         <nav className="flex flex-col gap-2 text-[white] text-sm pt-25 px-10">
-            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded" 
-            to="/Orders">Orders</Link>
+            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded inline-flex items-center gap-2"
+            to="/Orders">
+                <FaBox /> Orders</Link>
 
             <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-2 py-2 border-none rounded"
             to="/Cart">Cart</Link>
