00e1e5b2e67c2fe8b1e03549514f742b1e1edb3a
itsprinsprays <prncbntz@gmail.com>
2026-09-04 21:24:32 +0800

    Card Edited

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 export default function Card({ image, title, bgColor }) {
   return (
     <div 
-      className={`w-[25%] h-[300px] ${bgColor} items-center justify-center flex flex-col gap-2 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300`}
-    >
-      {image && <img src={image} alt={title} className="w-full h-[50%]" />}
-      <p className="text-lg font-semibold border-t w-full text-center">{title}</p>
+      className={`w-[25%] h-[400px] ${bgColor} items-center justify-center flex flex-col rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300`}>
+        {image && <img src={image} className="w-full h-1/2 object-cover" />}
+           <div className="h-1/2 flex items-center justify-center w-full border-t">
+        <p className="text-lg font-semibold text-center">{title}</p>
+      </div>
     </div>
   )
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,14 @@
 import hero from "../assets/hero.png"
+import sablay from "../assets/sablay.jpg"
 import Card from "./Card"
 
 export default function Dashboard() {
   return (
     <div className="flex flex-row gap-5 justify-center items-center mt-10">
-      <Card image={hero} title="Uniform" bgColor="bg-white" />
-      <Card image={hero} title="Orders" bgColor="bg-[#02E45B]" />
-      <Card title="Cart" bgColor="bg-[#02E49B]" />
-      <Card title="Profile" bgColor="bg-[#02E49B]" />
+      <Card image={hero} title="Polo" bgColor="bg-white" />
+      <Card image={sablay} title="Sablay" bgColor="bg-[white]" />
+      <Card title="NSTP Shirt" bgColor="bg-[#02E49B]" />
+      <Card title="PE Uniform" bgColor="bg-[#02E49B]" />
     </div>
   )
 }
