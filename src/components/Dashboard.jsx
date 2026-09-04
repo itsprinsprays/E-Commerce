48c99974f413de426d753e62dd478bffc5867bab
itsprinsprays <prncbntz@gmail.com>
2026-09-04 20:45:25 +0800

    Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,20 @@
+import hero from "../assets/hero.png"
+
 export default function Dashboard() {
 
     return (
         <>
-        <p>Helloo</p>
+    <div className="flex flex-row gap-5 justify-center items-center mt-10">
+        <div className="w-[25%] h-[300px] bg-[white] items-center justify-center flex flex-col gap-2 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
+            <img src={hero} alt="Hero" className="w-full h-[50%]"/>
+            <p className="text-lg font-semibold border-t-1 w-full">Uniform</p>
+        </div>
+
+        <div className="w-[25%] h-[200px] bg-[#02E49B]">Image</div>
+        <div className="w-[25%] h-[200px] bg-[#02E49B]">Image</div>
+        <div className="w-[25%] h-[200px] bg-[#02E49B]">Image</div>
+    </div>
+
         </>
     )
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,11 +10,11 @@ export default function Navigation() {
         <div className="bg-[green] w-[200px] h-screen flex flex-col py-10 ">
         <nav className="flex flex-col gap-2 text-[white] text-sm pt-25 px-10">
 
-            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded inline-flex items-center gap-2" 
+            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-2 py-2 border-none rounded inline-flex items-center gap-2" 
             to="/Dashboard">
                 <MdOutlineDashboardCustomize /> Dashboard</Link>
 
-            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded inline-flex items-center gap-2"
+            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-2 py-2 border-none rounded inline-flex items-center gap-2"
             to="/Orders">
                 <FaBox /> Orders</Link>
 
