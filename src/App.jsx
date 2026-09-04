7cbe07b8842ff9bb6f18a7614f058c7449ec3b51
itsprinsprays <prncbntz@gmail.com>
2026-09-04 20:03:53 +0800

    Dashboard Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 
 import './App.css'
@@ -6,6 +5,7 @@ import Login from "./components/Login"
 import Dashboard from './components/Dashboard'
 import Navigation from "./components/Navigation"
 import Layout from './components/Layout'
+import Notification from "./components/Notification"
 
 function App() {
  return (
@@ -14,7 +14,7 @@ function App() {
 
       <Route element={<Layout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/orders" element={<Dashboard />} />
+        <Route path="/orders" element={<Notification />} />
         <Route path="/cart" element={<Dashboard />} />
         <Route path="/profile" element={<Dashboard />} />
       </Route>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import { FaShoppingCart, FaBox, FaUser } from "react-icons/fa"
+import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { Link } from "react-router-dom"
 
 export default function Navigation() {
@@ -8,6 +9,11 @@ export default function Navigation() {
         <>
         <div className="bg-[green] w-[200px] h-screen flex flex-col py-10 ">
         <nav className="flex flex-col gap-2 text-[white] text-sm pt-25 px-10">
+
+            <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded inline-flex items-center gap-2" 
+            to="/Dashboard">
+                <MdOutlineDashboardCustomize /> Dashboard</Link>
+
             <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-3 py-2 border-none rounded inline-flex items-center gap-2"
             to="/Orders">
                 <FaBox /> Orders</Link>
@@ -17,7 +23,7 @@ export default function Navigation() {
                 <FaShoppingCart /> Cart</Link>
 
             <Link className="transition-colors duration-300 hover:bg-[#02E49B] px-2 py-2 border-none rounded inline-flex items-center gap-2"
-             to="/Cart">
+             to="/profile">
                 <FaUser /> Profile</Link>
         </nav>
         </div>
