47eca74c43349fc801b1c3a7968cc569c88880e6
itsprinsprays <prncbntz@gmail.com>
2026-09-06 21:34:56 +0800

    Change the Repeated JSX into Map

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,32 +1,24 @@
-import hero from "../assets/hero.png"
 import sablay from "../assets/sablay.jpg"
 import polo from "../assets/polo.jpg"
 import book from "../assets/book.jpg"
 import PE from "../assets/PE.jpg"
 import Card from "./Card"
 
+const products = [
+  { image: sablay, title: "Pajah Sablay", price: "PHP500", name: "Prince Benitez", gmail: "princejediel.benitez@cvsu.edu.ph" },
+  { image: polo, title: "Polo Shirt", price: "PHP300.99", name: "Renz Borromeo", gmail: "renz.borromeo@cvsu.edu.ph" },
+  { image: book, title: "NSTP Shirt", price: "$24.99", name: "Wendel Tuazon", gmail: "wendel.tuazon@cvsu.edu.ph" },
+  { image: PE, title: "PE Uniform", price: "$49.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
+  
+]
+
 export default function Dashboard() {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row gap-5 items-center mt-10">
-        <Card image={sablay} title="Pajah Sablay" price="PHP500" name="Prince Benitez" gmail="princejediel.benitez@cvsu.edu.ph" />
-        <Card image={polo} title="Polo Shirt" price="PHP300.99" name="Renz Borromeo" gmail="renz.borromeo@cvsu.edu.ph" />
-        <Card image={book} title="NSTP Shirt" price="$24.99" name="Wendel Tuazon" gmail="wendel.tuazon@cvsu.edu.ph" />
-        <Card image={PE} title="PE Uniform" price="$49.99" name="Robert Arpia" gmail="robert.arpia@cvsu.edu.ph" />
-      </div>
-
-      <div className="flex flex-row gap-5 items-center mt-10">
-               <Card image={sablay} title="Pajah Sablay" price="PHP500" name="Prince Benitez" gmail="princejediel.benitez@cvsu.edu.ph" />
-        <Card image={polo} title="Polo Shirt" price="PHP300.99" name="Renz Borromeo" gmail="renz.borromeo@cvsu.edu.ph" />
-        <Card image={book} title="NSTP Shirt" price="$24.99" name="Wendel Tuazon" gmail="wendel.tuazon@cvsu.edu.ph" />
-        <Card image={PE} title="PE Uniform" price="$49.99" name="Robert Arpia" gmail="robert.arpia@cvsu.edu.ph" />
-      </div>
-
-        <div className="flex flex-row gap-5 items-center mt-10">
-               <Card image={sablay} title="Pajah Sablay" price="PHP500" name="Prince Benitez" gmail="princejediel.benitez@cvsu.edu.ph" />
-        <Card image={polo} title="Polo Shirt" price="PHP300.99" name="Renz Borromeo" gmail="renz.borromeo@cvsu.edu.ph" />
-        <Card image={book} title="NSTP Shirt" price="$24.99" name="Wendel Tuazon" gmail="wendel.tuazon@cvsu.edu.ph" />
-        <Card image={PE} title="PE Uniform" price="$49.99" name="Robert Arpia" gmail="robert.arpia@cvsu.edu.ph" />
+        {products.map((item, index) => (
+          <Card key={index} {...item} />
+        ))}
       </div>
     </div>
   )
