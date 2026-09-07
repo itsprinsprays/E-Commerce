5afe512a6c69d97f4d7537ba9f0c7e73fb49b80b
itsprinsprays <prncbntz@gmail.com>
2026-09-07 21:39:31 +0800

    Index added for picture importing

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import sablay from "../assets/sablay.jpg"
+import polo from "../assets/polo.jpg"
+import book from "../assets/book.jpg"
+import PE from "../assets/PE.jpg"
+import PEPants from "../assets/PEPants.png"
+import CrimPantsBacoor from "../assets/CrimPantsBacoor.png"
+import EducUnif from "../assets/EducUnif.png"
+import EducUnif1 from "../assets/EducUnif1.png"
+import CrimPoloBacoor from "../assets/CrimPoloBacoor.png"
+import PsycUnif from "../assets/PsycUnif.png"
+
+export { sablay, polo, book, PE, PEPants, CrimPantsBacoor, CrimPoloBacoor, EducUnif, EducUnif1, PsycUnif };
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,17 @@
-import sablay from "../assets/sablay.jpg"
-import polo from "../assets/polo.jpg"
-import book from "../assets/book.jpg"
-import PE from "../assets/PE.jpg"
-import PEPants from "../assets/PEPants.png"
 import Card from "./Card"
+import { sablay, polo, book, PE, PEPants, CrimPantsBacoor, CrimPoloBacoor, EducUnif, EducUnif1, PsycUnif } from "../assets";
 
 const products = [
   { image: sablay, title: "Pajah Sablay", price: "PHP500", name: "Prince Benitez", gmail: "princejediel.benitez@cvsu.edu.ph" },
   { image: polo, title: "Polo Shirt", price: "PHP300.99", name: "Renz Borromeo", gmail: "renz.borromeo@cvsu.edu.ph" },
-  { image: book, title: "NSTP Shirt", price: "$24.99", name: "Wendel Tuazon", gmail: "wendel.tuazon@cvsu.edu.ph" },
-  { image: PE, title: "PE Uniform Set", price: "$49.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
-  { image: PEPants, title: "PE Pants", price: "200", name: "Ryhlle Cabatac", gmail: "ryhllevincent.cabatac@cvsu.edu.ph" }
+  { image: book, title: "NSTP Shirt", price: "PHP250.00", name: "Wendel Tuazon", gmail: "wendel.tuazon@cvsu.edu.ph" },
+  { image: PE, title: "PE Uniform Set", price: "PHP500.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
+  { image: PEPants, title: "PE Pants", price: "PHP200", name: "Ryhlle Cabatac", gmail: "ryhllevincent.cabatac@cvsu.edu.ph" },
+  { image: CrimPantsBacoor, title: "Criminology Pants", price: "PHP350.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" }, 
+  { image: EducUnif, title: "Education Uniform", price: "PHP400.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
+  { image: EducUnif1, title: "Education Uniform", price: "PHP400.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
+  { image: PsycUnif, title: "Psychology Uniform", price: "PHP300.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" },
+  { image: CrimPoloBacoor, title: "Criminology Polo", price: "PHP250.99", name: "Robert Arpia", gmail: "robert.arpia@cvsu.edu.ph" }
 ]
 
 export default function Dashboard() {
