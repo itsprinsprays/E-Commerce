ed7676095fa5bdb66cd1e007a28d876e53dcca31
itsprinsprays <prncbntz@gmail.com>
2026-09-21 18:44:24 +0800

    Sold Function

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -1,25 +1,35 @@
 import { CiUser } from "react-icons/ci";
 import { FaCheck } from "react-icons/fa";
+import { useState } from "react";
 
 export default function CardProfile({ image, title, price }) {
+
+    const [available, setAvailable] = useState(true);
+    const [availableText, setAvailableText] = useState("Available");
+
+    function sold() {
+        setAvailable(false);
+        setAvailableText("Sold")
+    }
 
     return (
     <>
         <div className="w-full bg-[#FFFFF2] flex flex-col rounded overflow-hidden hover:shadow-xl transition-shadow duration-300 gap-2 p-2 relative">
             {image && <img src={image} className="w-full h-40 sm:h-48 object-cover border-2 border-gray-300" />}
             
-            <h1 className="absolute text-white font-bold mx-2 shadow-2xl">Available</h1>
+            <h1 className="absolute text-white font-bold mx-2 shadow-2xl">{availableText}</h1>
 
             <div className="flex flex-wrap items-center justify-between gap-2">
                 <div className="flex flex-col min-w-0">
                     <h1 className="px-2 text-lg font-bold truncate">{title}</h1>
                     <p className="px-2 pb-2">{price}</p>
                 </div>
-
-                <button className="flex justify-center items-center gap-2 border w-full sm:w-auto border p-2 mt-2 truncate text-white rounded bg-[green] text-start tranistion-colors duration-300 hover:bg-[#02E49B] hover:text-black">
-                    <FaCheck className="shrink-0"/> <span className="">Mark as Sold</span> 
+                {available && 
+                <button onClick={sold} className="flex justify-center items-center gap-2 border w-full sm:w-auto border p-2 mt-2 truncate text-white rounded bg-[green] text-start tranistion-colors duration-300 hover:bg-[#02E49B] hover:text-black">
+                    <FaCheck className="shrink-0"/>Mark as Sold
 
                 </button>
+                } 
             </div>
 
         </div>
